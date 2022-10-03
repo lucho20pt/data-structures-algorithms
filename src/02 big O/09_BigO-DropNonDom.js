d270a6373097427d45fcbf2ee7713d90aa10ef86
Daniel Batista <dani_batista20@hotmail.com>
2022-10-03 12:02:15 +0100

    #2 Big O: #09 - Drop Non-Dominants

---
+++ src/02 big O/09_BigO-DropNonDom.js
@@ -0,0 +1,14 @@
+// Drop Non-Dominants
+// O(n^2) + O(n) <=> O(n^2 + n) <=> O(n^2)
+export function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j)
+    }
+  }
+  for (let k = 0; k < n; k++) {
+    console.log(k)
+  }
+}
+
+logItems(3)
