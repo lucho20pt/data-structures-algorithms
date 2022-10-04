00c613b9d309874e66e6c33e156a23c730223824
Daniel Batista <dani_batista20@hotmail.com>
2022-10-04 15:51:48 +0100

    #4 Data Structures - Linked Lists: #21 LL Push

---
+++ src/04 Data Structures - Linked Lists/21_LL-Push.js
@@ -0,0 +1,47 @@
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+// LinkedList class
+class LinkedList {
+  // constructor
+  constructor(value) {
+    const newNode = new Node(value)
+     
+    this.tail = this.head
+    this.length = 1
+  }
+
+  // push
+  push = (value) => {
+    const newNode = new Node(value)
+    // if LinkedList is empty
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+const myLinkedList = new LinkedList(7)
+// console.log(myLinkedList)
+// LinkedList {
+//   head: Node { value: 7, next: null },
+//   tail: Node { value: 7, next: null },
+//   length: 1 }
+
+myLinkedList.push(4)
+console.log(myLinkedList)
+// LinkedList { push: [λ: push],
+//   head: Node { value: 7, next: Node { value: 4, next: null } },
+//   tail: Node { value: 4, next: null },
+//   length: 2 }
