e6b54cae217ab8f90d3b56982bdc6ca495965f6c
Daniel Batista <dani_batista20@hotmail.com>
2022-10-06 22:43:10 +0100

    #5 Data Structures - Doubly Linked Lists: #35 DLL Unshift

---
+++ src/05 Data Structures - Doubly Linked Lists/35_DLL-Unshift.js
@@ -0,0 +1,82 @@
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+    this.prev = null
+  }
+}
+
+// DoublyLinkedList class
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value)
+    this.head = newNode
+    this.tail = this.head
+    this.length = 1
+  }
+
+  // push
+  push(value) {
+    const newNode = new Node(value)
+    // if empty list
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+
+  // pop
+  pop() {
+    // if empty list
+    if (this.length === 0) return undefined
+
+    const temp = this.tail
+
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = this.tail.prev
+      this.tail.next = null
+      temp.prev = null
+    }
+
+    this.length--
+
+    return temp
+  }
+
+  // unshift
+  unshift(value) {
+    const newNode = new Node(value)
+
+    if (this.length === 0) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+    }
+
+    this.length++
+
+    return this
+  }
+}
+
+const myDoublyLinkedList = new DoublyLinkedList(1)
+myDoublyLinkedList.push(2)
+
+// myDoublyLinkedList.pop()
+
+myDoublyLinkedList.unshift(555)
+
+console.log(myDoublyLinkedList)
