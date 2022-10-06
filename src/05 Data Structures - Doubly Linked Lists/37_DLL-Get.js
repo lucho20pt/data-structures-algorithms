15a05e545ace50d61315361d8fa06e338f3770ef
Daniel Batista <dani_batista20@hotmail.com>
2022-10-06 23:44:17 +0100

    #5 Data Structures - Doubly Linked Lists: #37 DLL Get

---
+++ src/05 Data Structures - Doubly Linked Lists/37_DLL-Get.js
@@ -0,0 +1,119 @@
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+    this.prev = null
+  }
+}
+
+// DoublyLinkedList class
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value)
+    this.head = newNode
+    this.tail = this.head
+    this.length = 1
+  }
+
+  // push
+  push(value) {
+    const newNode = new Node(value)
+    // if empty list
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+
+  // pop
+  pop() {
+    // if empty list
+    if (this.length === 0) return undefined
+
+    const temp = this.tail
+
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = this.tail.prev
+      this.tail.next = null
+      temp.prev = null
+    }
+
+    this.length--
+
+    return temp
+  }
+
+  // unshift
+  unshift(value) {
+    const newNode = new Node(value)
+
+    if (this.length === 0) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+    }
+
+    this.length++
+
+    return this
+  }
+
+  // shift
+  shift() {
+    if (this.length === 0) return undefined
+
+    const temp = this.head
+
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = this.head.next
+      this.head.prev = null
+      temp.next = null
+    }
+
+    this.length--
+
+    return temp
+  }
+
+  // get
+  get(index) {
+    if (index < 0 || index > this.length) return undefined
+
+    let temp = this.head
+    for (let i = 0; i < index; i++) {
+      temp = temp.next
+    }
+
+    return temp
+  }
+}
+
+const myDoublyLinkedList = new DoublyLinkedList(1)
+myDoublyLinkedList.push(2)
+myDoublyLinkedList.push(3)
+
+// myDoublyLinkedList.pop()
+
+// myDoublyLinkedList.unshift(555)
+
+// myDoublyLinkedList.shift()
+
+console.log(myDoublyLinkedList.get(1))
+
+// console.log(myDoublyLinkedList)
