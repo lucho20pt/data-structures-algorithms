23e9a5659fc4ab2412ea0aa9270d8a99b875ff75
Daniel Batista <dani_batista20@hotmail.com>
2022-10-13 19:23:02 +0100

    #12 Algorithms - Merge Sort: #85 Merge

--- src/12 Algorithms - Merge Sort/85_Merge.js
+++ src/12 Algorithms - Merge Sort/85_Merge.js
@@ -2,7 +2,7 @@ const array1 = [1, 3, 7, 8]
 const array2 = [2, 4, 5, 6]
 
 // Merge sorted arrays
-const merge = (arr1, arr2) => {
+export const merge = (arr1, arr2) => {
   let combined = []
   let i = 0
   let j = 0
@@ -30,6 +30,6 @@ const merge = (arr1, arr2) => {
   return combined
 }
 
-export const myMerge = merge(array1, array2)
+const myMerge = merge(array1, array2)
 
 console.log(myMerge)
