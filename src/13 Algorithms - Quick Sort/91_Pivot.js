bc15a21fa4bb43a130670b1ea830de89f7a79fba
Daniel Batista <dani_batista20@hotmail.com>
2022-10-14 00:21:36 +0100

    #13 Algorithms - Quick Sort: #91 Pivot

---
+++ src/13 Algorithms - Quick Sort/91_Pivot.js
@@ -0,0 +1,25 @@
+const arr = [4, 6, 1, 7, 3, 2, 5]
+
+// swap
+const swap = (array, firstIdx, secondIdx) => {
+  let temp = array[firstIdx]
+  array[firstIdx] = array[secondIdx]
+  array[secondIdx] = temp
+}
+
+// pivot
+const pivot = (array, pivotIdx = 0, endIdx = array.length - 1) => {
+  let swapIdx = pivotIdx
+  for (let i = pivotIdx + 1; i < endIdx; i++) {
+    if (array[i] < array[pivotIdx]) {
+      swapIdx++
+      swap(array, swapIdx, i)
+    }
+  }
+  swap(array, swapIdx, pivotIdx)
+  return swapIdx
+}
+
+const myPivot = pivot(arr)
+console.log(myPivot)
+console.log(arr)
